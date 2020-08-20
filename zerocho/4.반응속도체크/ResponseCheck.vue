d992f4a059d674b61cb465b37be42439931674f9
o-seun <61795897+o-seun@users.noreply.github.com>
2020-08-20 14:51:35 +0900

    first commit

---
+++ zerocho/4.반응속도체크/ResponseCheck.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <div id="screen">{{}}</div>
+    <div>
+      <div>평균 시간 : {{}}</div>
+    </div>
+    <button @:click="onReset">리셋</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+
+    }
+  },
+}
+  
+</script>
+
+<style>
+
+</style>
